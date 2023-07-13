231b72fb174874c358dcedfa4c07fcd63343c9f8
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-13 16:23:17 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char programming = "\"Programming is like building a multilingual puzzle";
+
+	puts(programming);
+
+	return (0);
+}
