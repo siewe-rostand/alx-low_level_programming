da5f19ce83b3fc561d6b37fc2d03489759cccbc4
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-14 16:46:33 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - combinations of two digits
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
