5077edabce4818d3b3df13c9a047f7cc28fa1074
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-18 02:08:47 -0700

    0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char str[] = "_puchar";
+
+	printf("%s\n", str);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ alx 0x02. C - Functions, nested loops by @siewe rostand
