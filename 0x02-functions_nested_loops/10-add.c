358c1c7f9352a0c5b42a20a93f67de0073fc535d
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-18 15:29:51 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two integers @siewe-rostand
+ * @n1: parameter 1
+ * @n2: parameter 2
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
