6c8e4e2abd2dba81f793678f15c35a7e9b6fc194
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-20 09:57:56 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,27 +9,23 @@
 
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i <= 100; i++)
+	for (n = 1; n < 101; n++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (n % 5 == 0 && n % 3 == 0)
 			printf("FizzBuzz ");
-		}
+		else if (n % 5 == 0)
+			if (n == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d ", n);
 	}
 	printf("\n");
+
 	return (0);
 }
