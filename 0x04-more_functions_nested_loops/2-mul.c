de1a75fc28725646c3962aa64c825a95b9ab84d1
siewe-rostand <rostand.siewe9@gmail.com>
2023-07-20 07:52:16 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplication @siewe-rostand
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: the product of the numbers
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@ int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
+int mul(int a, int b);
 
