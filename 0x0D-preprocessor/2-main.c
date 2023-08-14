f9f0f6f16d649c5e57d7cac326992efb45f23b61
siewe-rostand <rostand.siewe9@gmail.com>
2023-08-14 05:33:38 -0700

    2. file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: print the file name of the actual program
+ * Return: 0 if correct
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
