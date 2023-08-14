d5b5e2b7a2f1eec107ba9ee232b440a88e3b0773
siewe-rostand <rostand.siewe9@gmail.com>
2023-08-14 07:47:18 -0700

    0 poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - new type definition
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: last member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
