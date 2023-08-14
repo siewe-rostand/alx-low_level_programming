9f7d7d69ee8fd53f6cc93c48c7e2d3f449627203
siewe-rostand <rostand.siewe9@gmail.com>
2023-08-14 08:01:38 -0700

    1 init pointer to a struct type

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize struct dog
+ *
+ * @d: param of struct dog type
+ * @name: pointer to a char
+ * @age: float param
+ * @owner: pointer to a char param
+ *
+ * Return: nothing
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
