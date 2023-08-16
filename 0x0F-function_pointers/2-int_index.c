ae4d5c2aedc7c7e98c2a0da4fc653ead96f82993
siewe-rostand <rostand.siewe9@gmail.com>
2023-08-16 15:28:13 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,19 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
-	{
-		if ( size <= 0)
-		{
-			return (-1);
-		}
+	if (array == NULL || cmp == NULL || size < 0)
+		return (-1);
 
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (1);
-		}
-	}
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (1);
 
 	return (-1);
 }
