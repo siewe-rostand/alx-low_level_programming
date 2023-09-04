c7c816ce338727c3afbcc0a22d35ebd06fe151e6
siewe-rostand <rostand.siewe9@gmail.com>
2023-09-04 16:12:35 -0700

    0.read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
