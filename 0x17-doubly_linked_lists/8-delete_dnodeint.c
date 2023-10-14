4ea1710a7b2301931c2d2891f6cd46aa178e45cb
siewe-rostand <rostand.siewe9@gmail.com>
2023-10-13 17:26:29 -0700

    10. Palindromes

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,73 +1,52 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: head of doubly linked list
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
  *
- * Return: number of nodes
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	int count = 0;
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-
-/**
- * delete_dnodeint_at_index -  deletes the node at index of a linked list
- * @head: head of linked list
- * @index: index of node to delete
- *
- * Return: 1 if success, 0 if failure
- */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	size_t length;
-	unsigned int i = 0;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+	h1 = *head;
 
-	length = dlistint_len(*head);
-	if (index >= length)
-		return (-1);
-	temp = *head;
-	if (index == 0)
-	{
-		*head = temp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(temp);
-		return (1);
-	}
-	if (index == (length - 1))
-	{
-		while (temp->next)
-		{
-			temp = temp->next;
-		}
-		free(temp);
-	}
-	while (temp)
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
 		if (i == index)
 		{
-			temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
-			free(temp);
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-		temp = temp->next;
+		h2 = h1;
+		h1 = h1->next;
 		i++;
 	}
+
 	return (-1);
 }
