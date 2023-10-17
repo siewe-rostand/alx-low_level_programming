b82cc8856aae919e12590463320a289fcdcd8494
siewe-rostand <rostand.siewe9@gmail.com>
2023-10-16 17:11:00 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ *
+ * @s: param
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != 0)
+	{
+		length++;
+	}
+
+	return (length);
+}
